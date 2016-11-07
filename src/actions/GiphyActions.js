6c8425bef87d439c46697d4bd17f344e83707fd4
Richard Mands <richard@eduku.org>
2016-11-07 10:45:07 -0800

    fixed https

--- src/actions/GiphyActions.js
+++ src/actions/GiphyActions.js
@@ -4,7 +4,7 @@ export function fetchGiphy(search) {
   console.log('GiphyActions search: ', search);
   return {
     type: 'FETCH_GIPHY',
-    payload: axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=dc6zaTOxFJmzC  `)
+    payload: axios.get(`//api.giphy.com/v1/gifs/search?q=${search}&api_key=dc6zaTOxFJmzC  `)
                   .then(res => res.data),
   };
 }
